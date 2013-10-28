5111a56d898ae4619ba8c44cd6c3da93b0850bbf
Jason Pearson <jason.d.pearson@gmail.com>
2013-10-28 03:01:10 -0400

    created some simple configuration

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,39 @@
+
+# --- !Ups
+
+CREATE TABLE user (
+  id        BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email     VARCHAR(255) NOT NULL,
+  password  VARCHAR(255) NOT NULL,
+  salt      VARCHAR(255) NOT NULL,
+  created   DATETIME     NOT NULL
+);
+
+CREATE TABLE user_session (
+  id        BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user      BIGINT       NOT NULL,
+  token     VARCHAR(512) NOT NULL,
+  series    BIGINT       NOT NULL,
+  created   DATETIME     NOT NULL
+);
+
+CREATE TABLE login_attempt (
+  id        BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user      BIGINT       NOT NULL,
+  token     VARCHAR(512) NOT NULL,
+  series    BIGINT       NOT NULL,
+  created   DATETIME     NOT NULL
+);
+
+CREATE TABLE visit (
+  id        BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  uri       VARCHAR(255) NOT NULL,
+  userAgent TEXT         NULL,
+  user      BIGINT       NULL,
+  created   DATETIME     NOT NULL
+);
+
+# --- !Downs
+
+DROP TABLE user;
+DROP TABLE visit;
